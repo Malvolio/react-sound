24d3b962df53eddc15db98df1b7d4d9b835b4491
Malvolio <mlorton@yahoo.com>
2022-04-14 20:55:18 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-ifry2h
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-ifry2h)
---
+++ Sound.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+import useSound from "use-sound";
+
+const Sound: FC<{}> = () => {
+  return <div>Working</div>
+
+};
+
+export default Sound;
