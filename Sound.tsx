71023efe8ffa33ae0819fe313c75df8e28eb4b02
Malvolio <mlorton@yahoo.com>
2022-04-14 21:29:51 -0700

    Added stop button

--- Sound.tsx
+++ Sound.tsx
@@ -2,10 +2,32 @@ import React, { FC } from 'react';
 import useSound from 'use-sound';
 
 const Sound: FC<{}> = () => {
-  const [play] = useSound('/alarm.mp3');
+  const [playbackRate, setPlaybackRate] = React.useState(0.75);
+
+  const [play, { stop }] = useSound(
+    'https://cdn.jsdelivr.net/gh/Malvolio/react-sound@46196ba93c396389a2dd343fbe6482d57c45df3f/assets/alarm.mp3',
+    {
+      playbackRate,
+      // `interrupt` ensures that if the sound starts again before it's
+      // ended, it will truncate it. Otherwise, the sound can overlap.
+      interrupt: true,
+    }
+  );
+
+  const makeSqueaky = () => {
+    setPlaybackRate(playbackRate + 0.1);
+    play();
+  };
+
   return (
     <div>
       <button onClick={() => play()}>Play</button>
+      <button onClick={() => stop()}>Stop</button>
+      <button onClick={makeSqueaky}>
+        <span role="img" aria-label="Person with lines near mouth">
+          🗣
+        </span>
+      </button>
     </div>
   );
 };
